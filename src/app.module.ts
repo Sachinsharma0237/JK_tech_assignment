1ab631dc3b79e6ee8cad879ab195b61df16f1018
Your Name <your-email@example.com>
2025-02-24 17:38:56 +0530

    setup postgres database with password and username with dbName: jk_tech_db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: `${__dirname}/../.env` });
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, // Auto-create tables
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
